791033d1e11a7da92b5124939a30682967a411ae
AleksandrVareshkin <aleks5305858@gmail.com>
2022-07-24 16:32:59 +0300

    24.07 commit

--- wp-content/themes/smart/js/burger.js
+++ wp-content/themes/smart/js/burger.js
@@ -1,7 +1,8 @@
 let menuBtn = document.querySelector('.header__menu-btn');
 let menu = document.querySelector('.menu');
 let menuBox = document.querySelector('.menu-btn-box');
+let mainBlur = document.querySelector('.main');
 menuBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
-    menuBox.classList.toggle('active');
+    mainBlur.classList.toggle('active-blur');
 })
